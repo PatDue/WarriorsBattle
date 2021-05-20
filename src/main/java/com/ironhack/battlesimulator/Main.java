25cae2cc45209a749105db2adaa58ae081d37dc5
PatDue <mail@patrickduepmann.de>
2021-05-20 09:26:23 +0200

    main() aufgeräumt

--- src/main/java/com/ironhack/battlesimulator/Main.java
+++ src/main/java/com/ironhack/battlesimulator/Main.java
@@ -3,9 +3,7 @@ package com.ironhack.battlesimulator;
 public class Main {
 
     public static void main(String[] args) {
-        Party blackParty = new Party("Super Troopers");
-        Wizard.createManually();
-        blackParty.showMembers(true);
+
 
     }
 }
